37d1dac6336bc49f9d6a5ba1239403f041c39ad0
Maaruf199 <maaruf_kassim@live.com.sg>
2020-08-05 14:12:25 +0800

    Post Model/Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/destroy'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do 
+    resources :posts
 end
+root 'posts#index' # home page on localhost:3000
+ end
