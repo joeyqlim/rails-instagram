4e59cda82d84a607f4d745ff471f9a54c9d5f5d5
akjw <aseykoh@gmail.com>
2020-08-05 21:04:18 +0800

    fix follow duplicates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.all
+    @follow_list = current_user.followships.pluck(:following_id)
   end
 
   def show
