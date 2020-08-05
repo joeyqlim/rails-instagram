94701c1435a1b4d5012f7752cfbf12645caead94
akjw <aseykoh@gmail.com>
2020-08-05 21:21:58 +0800

    fix indentations for follow fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.all
+    @followships = Followship.all
     @follow_list = current_user.followships.pluck(:following_id)
   end
 
