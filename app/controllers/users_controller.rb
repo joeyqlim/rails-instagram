155e38d8e7f96ed73b4fefbd7b77799515085582
akjw <aseykoh@gmail.com>
2020-08-05 17:09:50 +0800

    add set bio functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,23 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_bio)
+      redirect_to user_path(current_user)
+    else 
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_bio
+    params.require(:user).permit(:biography)
+  end
 end
