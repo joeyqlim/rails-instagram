5b6866180110eb9e60b50c4ee2d1f4472ee477de
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-08-05 14:31:27 +0800

    generate comments model, views, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
