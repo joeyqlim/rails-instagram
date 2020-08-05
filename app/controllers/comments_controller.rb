68e6a629b23649671f1fcec636c153913fc902e1
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-08-05 22:07:36 +0800

    comments done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,29 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :new_comment, only: [:new, :create]
+  before_action :set_comment, only: [:show, :edit]
+
   def index
+    @comments = Comment.all
   end
 
   def show
   end
 
   def new
+    @comment = @post.comments.build
+    # @comment["user_id"] = current_user.id 
   end
 
   def create
+    @comment = @post.comments.build(comment_params)
+    @comment["user_id"] = current_user.id 
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +33,19 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def new_comment
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
